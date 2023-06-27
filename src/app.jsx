3c71d28cd807e7c64f9525028d4e3a724b347066
NusratJahann <dilerakhatun52@gmail.com>
2023-06-27 21:27:34 +0600

    theme added

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,13 @@
-import './app.css'
+import "./app.css";
 
 export function App() {
-
   return (
-    <div>
-      
+    <div className="flex items-center justify-center h-[80vh]">
+      <span className="countdown text-9xl">
+        <span style={{ "--value": 10 }}></span>: 
+        <span style={{ "--value": 24 }}></span>: 
+        <span style={{ "--value": 36 }}></span>
+      </span>
     </div>
-  )
-};
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
-}
-
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+          "primary": "#6eff6a",
+          "primary-focus": "#7dff79",
+          "neutral": "#131413",
+          "neutral-focus": "#1e1f1e",
+          "base-100": "#060706",
+          "base-content": "#ffffff",
+          "text-lg": "1rem",
+          
+          "--rounded-btn": "0.9375rem",
+          "--rounded-badge": "1.9rem", 
+          "--animation-btn": "0.25s", 
+          "--animation-input": "0.2s", 
+          "--btn-text-case": "lowercase", 
+          "--btn-focus-scale": "0.95", 
+          "--border-btn": "5px", 
+        },
+      },
+    ],
+  },
+};
